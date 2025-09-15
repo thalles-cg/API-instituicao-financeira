0fdcb6e9cff864d44049186fa6ac1886cbe31eb6
thalles-cg <thalles.pity@gmail.com>
2025-09-15 08:45:59 -0300

    feat: implementação do validador de CPF

---
+++ src/services/cpfValidatorService.js
@@ -0,0 +1,20 @@
+export function isValidCPF(cpf) {
+  if (typeof cpf !== 'string') return false;
+  cpf = cpf.replace(/[^\d]+/g, '');
+  if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) return false;
+
+  let soma = 0, resto;
+
+  for (let i = 1; i <= 9; i++) soma += parseInt(cpf[i - 1]) * (11 - i);
+  resto = (soma * 10) % 11;
+  if (resto >= 10) resto = 0;
+  if (resto !== parseInt(cpf[9])) return false;
+
+  soma = 0;
+  for (let i = 1; i <= 10; i++) soma += parseInt(cpf[i - 1]) * (12 - i);
+  resto = (soma * 10) % 11;
+  if (resto >= 10) resto = 0;
+  if (resto !== parseInt(cpf[10])) return false;
+
+  return true;
+}
