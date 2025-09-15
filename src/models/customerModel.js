8232c491c1eac95d81723d396cb57fda64969363
thalles-cg <thalles.pity@gmail.com>
2025-09-15 08:57:33 -0300

    feat(customer): implementação da validação de CPF no schema e importação do counter

--- src/models/customerModel.js
+++ src/models/customerModel.js
@@ -1,34 +1,40 @@
 import mongoose from 'mongoose';
+import { isValidCPF } from '../services/cpfValidatorService.js';
+import CounterService from '../services/counterService.js';
 const { Schema, model } = mongoose;
 
-const counterSchema = new Schema({
-  _id: { type: String, required: true },
-  seq: { type: Number, default: 0 }
-});
-
-const Counter = model('Counter', counterSchema);
-
 const customerSchema = new Schema({
-  _id: { type: String }, 
+  _id: { type: String },
   name: { type: String, required: true },
-  cpf: { type: String, required: true, unique: true },
+  cpf: { 
+    type: String, 
+    required: true, 
+    unique: true,
+    validate: {
+      validator: isValidCPF,
+      message: 'Invalid CPF'
+    }
+  },
   email: { type: String, required: true, unique: true },
-  accounts: { type: [String], default: [] }
+  accounts: [{ type: String, ref: 'Account' }]
+}, {
+  timestamps: true
 });
 
-customerSchema.pre('save', async function (next) {
+customerSchema.pre('save', async function(next) {
   if (this.isNew) {
-    const counter = await Counter.findByIdAndUpdate(
-      { _id: 'customerId' },      
-      { $inc: { seq: 1 } },       
-      { new: true, upsert: true } 
-    );
-
-    const seqNumber = counter.seq.toString().padStart(3, '0'); 
-    this._id = `cus_${seqNumber}`;
+    try {
+      const seqNumber = await CounterService.getNextSequence('customer');
+      this._id = `cus_${seqNumber}`;
+      next();
+    } catch (err) {
+      next(err); 
+    }
+  } else {
+    next();
   }
-  next();
 });
+
 
 const Customer = model('Customer', customerSchema);
 
