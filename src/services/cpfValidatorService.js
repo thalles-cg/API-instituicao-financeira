2aa1839c2b15cdcfa8796ded7db6255d3f2fa8c3
thalles-cg <thalles.pity@gmail.com>
2025-09-16 11:55:50 -0300

    refactor(validation): traduz variáveis do validador de CPF para inglês

--- src/services/cpfValidatorService.js
+++ src/services/cpfValidatorService.js
@@ -3,18 +3,18 @@ export function isValidCPF(cpf) {
   cpf = cpf.replace(/[^\d]+/g, '');
   if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) return false;
 
-  let soma = 0, resto;
+  let sum = 0, remainder;
 
-  for (let i = 1; i <= 9; i++) soma += parseInt(cpf[i - 1]) * (11 - i);
-  resto = (soma * 10) % 11;
-  if (resto >= 10) resto = 0;
-  if (resto !== parseInt(cpf[9])) return false;
+  for (let i = 1; i <= 9; i++) sum += parseInt(cpf[i - 1]) * (11 - i);
+  remainder = (sum * 10) % 11;
+  if (remainder >= 10) remainder = 0;
+  if (remainder !== parseInt(cpf[9])) return false;
 
-  soma = 0;
-  for (let i = 1; i <= 10; i++) soma += parseInt(cpf[i - 1]) * (12 - i);
-  resto = (soma * 10) % 11;
-  if (resto >= 10) resto = 0;
-  if (resto !== parseInt(cpf[10])) return false;
+  sum = 0;
+  for (let i = 1; i <= 10; i++) sum += parseInt(cpf[i - 1]) * (12 - i);
+  remainder = (sum * 10) % 11;
+  if (remainder >= 10) remainder = 0;
+  if (remainder !== parseInt(cpf[10])) return false;
 
   return true;
 }
