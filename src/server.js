ee750e27f8354dbc764fe1c390fccbbe28902a23
thalles-cg <thalles.pity@gmail.com>
2025-11-11 10:51:53 -0300

    feat: adiciona mais tempo para o conexão com o db

--- src/server.js
+++ src/server.js
@@ -7,7 +7,13 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 const MONGO_URL = process.env.MONGO_URL;
 
-mongoose.connect(MONGO_URL)
+const connectOptions = {
+  connectTimeoutMS: 30000,          
+  serverSelectionTimeoutMS: 30000,
+  socketTimeoutMS: 45000            
+};
+
+mongoose.connect(MONGO_URL, connectOptions)
   .then(() => {
     console.log("Database connected");
     app.listen(PORT, () => {
