26c18dd004e58803b0d957aa0439cb2dd4e7d9ba
thalles-cg <thalles.pity@gmail.com>
2025-11-13 09:24:19 -0300

    feat(api): adiciona endpoint de descoberta (index) em /openfinance

--- src/routes/openfinanceRoute.js
+++ src/routes/openfinanceRoute.js
@@ -6,6 +6,31 @@ import consentRoute from "./consentRoute.js";
 
 const openfinanceRoute = express.Router();
 
+openfinanceRoute.get("/", (req, res) => {
+   res.status(200).json({
+      message: "API IF-Thalles", 
+      version: "1.0.0", 
+      status: "online",
+      endpoints: {
+      public: [
+         "GET /openfinance/",
+         "GET /openfinance/customers/lookup/by-cpf/:cpf"
+      ],
+      consent: [
+         "POST /openfinance/consents",
+         "GET /openfinance/consents/:consentId",
+         "DELETE /openfinance/consents/:consentId"
+      ],
+      protected: [
+         "GET /openfinance/customers/:customerId",
+         "GET /openfinance/customers/:customerId/accounts",
+         "GET /openfinance/accounts/:accountId/balance",
+         "GET /openfinance/transactions/:accountId"
+      ]
+      }
+   });
+});
+
 openfinanceRoute.use("/customers", customerRoute);
 openfinanceRoute.use("/accounts", accountRoute);
 openfinanceRoute.use("/transactions", transactionRoute);
